a7ad43dc796ff2a3cd562e321b35359f88938505
Bakustar <zhumadil.bakustar@gmail.com>
2023-03-23 19:10:45 +0600

    commit1

--- tsis6/dir_and_files/6.py
+++ tsis6/dir_and_files/6.py
@@ -5,10 +5,11 @@ prefix_path = r'/Users/bakustar2005/Documents/pp2/tsis6/dir_and_files/files'
 path = "{}/{}.txt"
 
 for i in range(65, 91):
+    
     dir_path = path.format(prefix_path, chr(i))
+    
     if os.path.exists(dir_path):
         os.remove(dir_path)
-
-for i in range(65, 91):
-    dir_path = path.format(prefix_path, chr(i))
+    
     file = open(dir_path, 'x')
+    
--- tsis6/dir_and_files/8.py
+++ tsis6/dir_and_files/8.py
@@ -1,7 +1,11 @@
-#Write a Python program to delete file by specified path. Before deleting check for access and whether a given path exists or not.
+#Write a Python program to delete file by specified path. 
+#Before deleting check for access and whether a given path exists or not.
+
 import os
 
 if os.path.exists(r'/Users/bakustar2005/Documents/pp2/tsis6/dir_and_files/todelete.txt'):
+    
     os.remove(r'/Users/bakustar2005/Documents/pp2/tsis6/dir_and_files/todelete.txt')
+
 else:
     print("The file does not exist")
