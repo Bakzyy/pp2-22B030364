a4a7ba2f17473732e9b60d93b622a7226aae74de
Bakustar <zhumadil.bakustar@gmail.com>
2023-03-23 14:18:09 +0600

    commit1

--- tsis6/builtin_functions/2.py
+++ tsis6/builtin_functions/2.py
@@ -1,5 +1,4 @@
 #Write a Python program with builtin function that accepts a string and calculate the number of upper case letters and lower case letters
-s = "Write a Python program with builtin function that accepts a string and calculate the number of upper case letters and lower case letters"
 
 s = str(input())
 
--- tsis6/dir_and_files/3.py
+++ tsis6/dir_and_files/3.py
@@ -5,8 +5,11 @@ path = input()
 path1 = r'/Users/bakustar2005/Documents/pp2/tsis6/dir_and_files/demofile.txt'
 
 if os.path.exists(path):
+    
     x = os.path.split(path)
+    
     print("Filename:", x[1])
     print("Directory portion:", x[0])
+
 else:
     print("Path does not exists")
